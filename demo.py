3fe0f8830b0b58b07aab76f5f2dccef65292a2bb
Zhang1933 <193319076@qq.com>
2020-05-14 22:03:13 +0800

    1.把一些没有用的代码删去了。

--- demo.py
+++ demo.py
@@ -1,19 +1,13 @@
 from pathlib import Path
-import struct
 
 judge={".jpg":b"\xff\xd8",".png": b"\x89P", ".bmp":b"BM",".pdf":b"%P",".docx":b"PK",".pptx":b"PK",".exe":b"MZ"}
 # 生成字典匹配，比较前两位（因为只有前两位的不同）
 
 def check(suf,fileHeader):
-    if judge[suf]==fileHeader:
-        return 1
-    else:
-        return 0
+    return judge[suf]==fileHeader
 
 def moveFlies(file,desfile):
     Path(file).rename(desfile/Path(file).name)
-
-
 
 root=input("输入要查错的目录:")
 
@@ -32,21 +26,23 @@ Path(RFlies).mkdir(exist_ok=True)
 
 files=Path(root).glob("**/*")
 
-tfiles={}
-for file in files:
-    if Path(file).is_file():
-        st=Path(file).stat()
-        tfiles[file]=st.st_size
 
-files={k:v for k,v in sorted(tfiles.items(),key=lambda x: x[1],reverse=True)}
+# tfiles={}
+# for file in files:
+#     if Path(file).is_file():
+#         st=Path(file).stat()
+#         tfiles[file]=st.st_size
+
+# files={k:v for k,v in sorted(tfiles.items(),key=lambda x: x[1],reverse=True)}
 # 只是熟练一下排序，没有意义的排序
 
 for file in files:
     # print(file)
-    
-    
-    suf=Path(file).suffix
+    if Path(file).is_file()== False:
+        continue
 
+    suf=Path(file).suffix
+    
     with open(file,'rb') as f:
         fileHeader=f.read(2)
     #读取文件头前两位比较
